8553667d91623e77104df77353dbefc9615a5975
Ming-der Wang <mingderwang@gmail.com>
2023-12-03 00:17:46 +0800

    init, ok

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 console.log("Hello via Bun!");
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dhayfcoyzngyiodlgceh.supabase.co'
+const supabaseUrl = 'https://okxythyrzmhrkfuiahgn.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
